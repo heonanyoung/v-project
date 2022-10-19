7d659277fa3b5a12262fd2bcdbf2a219ad933eb6
heonanyoung <115797444+heonanyoung@users.noreply.github.com>
2022-10-19 22:33:01 +0900

    Update vgan.py

--- vgan.py
+++ vgan.py
@@ -1,3 +1,8 @@
+#프로그램 세팅
+# python -m pip install --upgrade pip wheel setuptools 
+# python -m pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew
+# python -m pip install kivy.deps.gstreamer
+# python –m pip install kivy 
 import kivy
 import os
 from kivy.app import App
@@ -12,13 +17,18 @@ from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 
 
 kivy.require('1.11.1')
-
 fontname = 'NanumBarunGothic.ttf'
 
-class Upper_bar(BoxLayout): #상단바
-    pass
 
-class PageLayout(PageLayout):
+class PageLayout(PageLayout): #페이지 구분
+    def selected(self, filename):
+        try:
+            self.ids.my_image.source = filename[0]
+            print(filename[0])
+        except:
+            pass
+
+class Upper_bar(BoxLayout): #상단바
     pass
 
 class VeganApp(App): #어플이름
